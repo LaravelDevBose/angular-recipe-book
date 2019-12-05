829f96e4029e60061b6b82ffda27345f1cb53ce5
laraveldevbose <laravel.devbose@gmail.com>
2019-12-06 00:08:32 +0600

    show data on recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: "app-recipe-list",
@@ -6,6 +7,19 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe', 
+      'This is a Test Recipe', 
+      'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Crema-Recipe.jpg',
+    true),
+    new Recipe(
+      'Test Recipe', 
+      'This is a Test Recipe', 
+      'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Crema-Recipe.jpg',
+    true)
+  ];
+
   constructor() {}
 
   ngOnInit() {}
