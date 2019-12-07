bc6109efa741dc251d5b667f600d7813bd160e1c
tokingfisher <tokingfisher19@gmail.com>
2019-12-07 11:11:48 +0600

    project basis part done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('tomato', 100),
+    new Ingredient('Onion', 100),
+    new Ingredient('Apple', 100),
+  ];
   constructor() { }
 
   ngOnInit() {
